38fc11a28d4f4bdbc6cc4dd60bfa66903ba926f4
azki0102-afk <azki0102@student.abudzar.sch.id>
2026-02-17 08:46:32 +0700

    first commit

---
+++ manipulasidom/dom.js
@@ -0,0 +1,37 @@
+console.log("hai apa kabar ");
+
+let hallo = document.getElementById("mydiv");
+
+ console.dir(hallo);
+
+console.log(hallo.textContent);
+
+console.dir(document);
+
+let box = document.getElementById("myspan");
+
+console.log(box.textContent);
+
+console.dir(document);
+
+box.textContent = "hadfidz";
+console.log(box.textContent);
+
+let text = document.getElementById("content");
+text.textContent = 20;
+
+let paragraphElement = document.createElement("p");
+
+document.getElementById("container").appendChild;
+
+paragraphElement;
+
+paragraphElement.textContent = "langit cerah";
+
+document.getElementById("container").appendChild(paragraphElement);
+
+let number = document.getElementById("phone");
+console.log(number.value);
+
+let gender = document.getElementById("gender");
+console.log("number");
---
+++ tugas-7/tugas.js
@@ -0,0 +1,7 @@
+let gender = document.getElementById("gender");
+console.log("number");
+
+ let contactNumber = document.getElementById("contactNumber");
+contactNumber.value = "08123456789";
+
+let articleContainer = document.getElementById('addParagraphBtn')
