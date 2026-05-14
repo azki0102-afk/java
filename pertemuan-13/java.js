8ab26ed007eab572da6eed1c8985a23d9fe5e6cf
azki0102-afk <azki0102@student.abudzar.sch.id>
2026-05-14 10:00:18 +0700

    first commit

---
+++ pertemuan-13/java.js
@@ -0,0 +1,22 @@
+let name = null;
+let username = name ?? 'tidak memiliki nama';
+
+let c = '';
+let result = c ?? 'hacker';
+console.log(result);
+
+let appel = = null ;
+let grap = null;
+let watermelon = null;
+let blubery = 8;
+console.log (appel ?? grap ?? blubery ?? watermelon);
+
+let appel1 = null;
+
+let grap1 = null ;
+
+let watermelon1 = '0';;
+
+let blubery1 = '';
+
+console.log(appel1 || grap1 || blubery1  || watermelon1  );
---
+++ tugas-37/java.js
@@ -0,0 +1,11 @@
+let apel = null;
+let mangga = '';
+let jeruk = "Jeruk Ada";
+
+console.log(apel || mangga || jeruk);
+
+let teh = null;
+let kopi = undefined;
+let susu = 10;
+
+console.log(teh ?? kopi ?? susu);
